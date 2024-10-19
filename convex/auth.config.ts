a5c816376a1aca8d3a92b736ebb899871992119e
parthacs1999 <partha.c1999@gmail.com>
2024-10-19 08:40:02 +0530

    Clerk set up

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    providers: [
+      {
+        domain: "https://powerful-grubworm-12.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
