2681bde1d02c11054351c49d68c8190bea652b52
parthacs1999 <partha.c1999@gmail.com>
2024-11-21 07:46:51 +0530

    Added components with logic for workflow

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,16 +1,42 @@
-'use client';
-import React from 'react';
+"use client";
+
+import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
-export function Providers ({ children } : { children: React.ReactNode }) {
-    const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
-    return (
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-                {children}
-            </ConvexProviderWithClerk>
-        </ClerkProvider>
-    )
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL as string
+);
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <OtherProviders>{children}</OtherProviders>
+    </ThemeProvider>
+  );
+}
+
+function OtherProviders({ children }: { children: React.ReactNode }) {
+  const { resolvedTheme } = useTheme();
+
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: resolvedTheme === "dark" ? dark : undefined,
+      }}
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 }
--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,9 +1,8 @@
-/* eslint-disable import/no-anonymous-default-export */
 export default {
-    providers: [
-      {
-        domain: "https://powerful-grubworm-12.clerk.accounts.dev",
-        applicationID: "convex",
-      },
-    ]
-  };
+  providers: [
+    {
+      domain: "https://perfect-cricket-75.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ]
+};
